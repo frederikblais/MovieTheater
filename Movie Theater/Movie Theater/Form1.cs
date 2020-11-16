38f1f1c40d36c77f28c032f48d758c5ab0e47569
William Andrews <LE1770646@crc-lennox.qc.ca>
2020-11-16 11:56:34 -0500

    DB connection

---
+++ Movie Theater/Movie Theater/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Movie_Theater
+{
+    public partial class Form1 : Form
+    {
+        private const string DbServerHost = "localhost";
+        private const string DbUsername = "postgres";
+        private const string DbUuserPassword = "1013";
+        private const string DbName = "movietheaterdb";
+
+        NpgsqlConnection dbConnection;
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetDBConnection(DbServerHost, DbUsername, DbUuserPassword, DbName);
+
+            checkPostgresVersion();        
+        }
+
+        private void SetDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            string connectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(connectionString);
+        }
+
+        private NpgsqlConnection CreateDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            string conectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(conectionString);
+
+            return dbConnection;
+        }
+
+        private void checkPostgresVersion()
+        {
+            //Before sending commands to the database, the connection must be opened
+            dbConnection.Open();
+
+            //This is a string representing the SQL query to execute in the db
+            string sqlQuery = "SELECT version()";
+
+
+            //This is the actual SQL containing the query to be executed
+            NpgsqlCommand dbCommand = new NpgsqlCommand(sqlQuery, dbConnection);
+
+            //This variable stores the result of the SQL query sent to the db
+            string postgresVersion = dbCommand.ExecuteScalar().ToString();
+
+            //After executing the query(ies) in the db, the connection must be closed
+            dbConnection.Close();
+
+            Console.WriteLine("\n----------------------");
+
+            Console.WriteLine("PostgreSQL version: " + postgresVersion);
+        }
+    }
+}
