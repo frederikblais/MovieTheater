5532bea275d6a02599fcbecf4c400fdba07ae4f5
William Andrews <LE1770646@crc-lennox.qc.ca>
2020-11-18 15:49:13 -0500

    added database

--- Movie Theater/Movie Theater/ManagerLogin.cs
+++ Movie Theater/Movie Theater/ManagerLogin.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Movie_Theater
 {
     public partial class ManagerLogin : Form
     {
+        private const string DbServerHost = "localhost";
+        private const string DbUsername = "postgres";
+        private const string DbUuserPassword = "1013";
+        private const string DbName = "movietheaterdb";
+
+        NpgsqlConnection dbConnection;
         public ManagerLogin()
         {
             InitializeComponent();
+
+            SetDBConnection(DbServerHost, DbUsername, DbUuserPassword, DbName);
+        }
+
+        private void SetDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            string connectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(connectionString);
+        }
+
+        private NpgsqlConnection CreateDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            string conectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(conectionString);
+
+            return dbConnection;
         }
 
         private void loginManagerButton_Click(object sender, EventArgs e)
